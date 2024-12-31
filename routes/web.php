f48351962703ee3c1821137d3dd20e0a8b5fc5f5
7avir <tanvir@g.bracu.ac.bd>
2024-12-31 13:19:45 +0600

    fix: navigation bar added and route updated.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\ResultController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\CartController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -43,5 +47,38 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/products', [ProductController::class, 'index'])->name('products');
+
+Route::get('/product/{product_id}', function($product_id) {
+    // Fetch the product by ID using the query builder
+    $product = DB::table('products')->where('product_id', $product_id)->first();
+
+    if (!$product) {
+        abort(404, 'Product not found');
+    }
+
+    return view('productdetails', compact('product'));
+})->name('product.details');
+
+Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.index');
+
+Route::post('/product/{product_id}/add-to-cart', [CartController::class, 'addToCart'])->name('product.addToCart');
+Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
+Route::post('/cart/update/{product_id}', [CartController::class, 'updateQuantity'])->name('cart.update');
+Route::delete('/cart/remove/{product_id}', [CartController::class, 'removeItem'])->name('cart.remove');
+
+
+Route::get('/cart/checkout', function () {
+    return view('cart.checkout');
+})->name('checkout');
+
+Route::post('/cart/checkout/complete', function () {
+    session()->flash('success', 'Your Purchase is complete!');
+    return redirect()->route('checkout'); // Redirect back to the checkout page
+})->name('checkout.complete');
+
+Route::get('/product/{id}/image', [ProductController::class, 'show'])->name('product.image');
+
 
 require __DIR__.'/auth.php';
